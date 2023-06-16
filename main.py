1d104d409c1578eb19427bd8ee5ad18e5c151975
Shizuka <tkpp26@gmail.com>
2023-06-15 18:51:07 -0700

    Edit main.py to print message

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ def main():
     ##############################
     # make your code below
     # print('Hello World')
-    ##############################
-
+    ##############################s
+    print('Hello World')
+    print('Python Programming')
 
 if __name__ == '__main__':
     main()
